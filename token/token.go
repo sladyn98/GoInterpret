9f8147268a7efbf4e89801d4eaf2c7c600555025
Sladyn <gunnerforlife00@gmail.com>
2019-09-12 00:41:30 +0530

    Added lexer test and token types

--- token/token.go
+++ token/token.go
@@ -6,3 +6,30 @@ type Token struct {
 	Type 	TokenType
 	Literal string
 }
+
+const (
+
+	ILLEGAL = "ILLEGAL"
+	EOF 	= "EOF"
+
+	//Identifiers and Literals
+	IDENT   = "IDENT"
+	INT     = "INT"
+
+	//Operators
+	ASSIGN  = "="
+	PLUS    = "+"
+
+	//Delimiters
+	COMMA   	= ","
+	SEMICOLON 	= ";"
+
+	LPAREN     	= "("
+	RPAREN     	= ")"
+	LBRACE     	= "{"
+	RBRACE     	= "}"
+
+	//Keywords
+	FUNCTION   	= "FUNCTION"
+
+)
