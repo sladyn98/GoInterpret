d0554f457fbf3ef81fc24e97ab1ab002b30a9bb6
Sladyn <gunnerforlife00@gmail.com>
2019-09-16 18:48:24 +0530

    Added new parsing functions and travis.yml

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -17,11 +17,11 @@ func TestNextToken(t *testing.T)  {
 		{token.PLUS, "+"},
 		{token.LPAREN, "("},
 		{token.RPAREN, ")"},
+		{token.LBRACE,"{"},
+		{token.RBRACE,"}"},
 		{token.COMMA, ","},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
-		{token.LBRACE,"{"},
-		{token.RBRACE,"}"},
 	}
 
 	l := New(input)
@@ -30,6 +30,7 @@ func TestNextToken(t *testing.T)  {
 		tok := l.NextToken()
 
 		if tok.Type  != tt.expectedType {
+
 			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
 				i, tt.expectedType, tok.Type)
 		}
